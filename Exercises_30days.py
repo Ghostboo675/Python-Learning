1fbb575be5203394fd47b6bf2f105314abc4cf4c
Notoriousnether <samuelo120610@gmail.com>
2026-07-28 15:20:45 +0100

    Day 2 Exercises

---
+++ Exercises_30days.py
@@ -0,0 +1,29 @@
+# day 1 introduction
+from ast import compare
+
+# day 2 variables
+
+# level 1
+print("Day 2: 30 Days of python programming")
+First_name = "Samuel"
+Last_name = "Omoruyi"
+Full_name = First_name + " " + Last_name
+Country = "England"
+City = "Sheffield"
+Age = 16
+year = 2017
+is_married = True
+is_true = True
+is_light = False
+best_years = [2017, 2020, 2022, 2025, 2026]
+
+# level 2
+print(type(First_name))
+print(type(Last_name))
+print(type(Age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(best_years))
+print(len(First_name))
+print(len(Last_name))
