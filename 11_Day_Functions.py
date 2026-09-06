2dbcaab069c7b554900306df81869ce7900ce50f
Notoriousnether <samuelo120610@gmail.com>
2026-09-06 17:52:29 +0100

    Day 11 Intro

---
+++ 11_Day_Functions.py
@@ -0,0 +1,9 @@
+def add_two_numbers(num1, num2):
+    total = num1 + num2
+    return total
+print(add_two_numbers(2, 5))
+
+def area_of_circle(pi, radius):
+    area = pi * radius * radius
+    return area
+print(area_of_circle(3.14, 5))
